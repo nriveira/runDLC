05319a55a5671af3f1cf498ec678f4d02e79e12e
nriveira <47254070+nriveira@users.noreply.github.com>
2021-02-07 19:33:35 -0600

    Add files via upload

---
+++ container.sh
@@ -0,0 +1,5 @@
+singularity shell --nv --home $PWD \
+		  -B local_models/:/usr/local/lib/python3.6/dist-packages/deeplabcut/pose_estimation_tensorflow/models/pretrained/ \
+		  colginlab_latest.sif 
+
+export DLClight=True
