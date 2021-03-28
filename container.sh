9527a71b35bf633b19b8b930371891885c7cca1b
nriveira <47254070+nriveira@users.noreply.github.com>
2021-03-28 01:42:15 -0500

    Update container.sh

--- container.sh
+++ container.sh
@@ -2,4 +2,4 @@ singularity shell --nv --home $PWD \
 		  -B local_models/:/usr/local/lib/python3.6/dist-packages/deeplabcut/pose_estimation_tensorflow/models/pretrained/ \
 		  colginlab_latest.sif 
 
-export DLClight=True
+pip install deeplabcutcore
