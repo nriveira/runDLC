7f296bcd61c304e4d12ea7218238ac2d4880d3ef
nriveira <47254070+nriveira@users.noreply.github.com>
2021-03-28 01:43:09 -0500

    Update to include deeplabcutcore

--- runDLC.py
+++ runDLC.py
@@ -1,7 +1,6 @@
 import sys
-import deeplabcut
+import deeplabcutcore as deeplabcut
 
-print(sys.argv[1])
 config_path = sys.argv[1]
 deeplabcut.create_training_dataset(config_path)
 deeplabcut.train_network(config_path)
